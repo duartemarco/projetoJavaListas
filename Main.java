54165b1fe47b938bb48112efd2b0343ee9c9c02b
Marco Túlio Duarte <109878566+duartemarco@users.noreply.github.com>
2023-04-15 17:43:53 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,49 +7,84 @@ public class Main {
 		// criando uma lista sequencial de alunos
 		ListaSequencial listasequencial = new ListaSequencial();
 
-//		System.out.println("Quantos alunos você deseja cadastrar?");
-
 		System.out.println(
-				"1- Cadastrar aluno \n2- Buscar aluno via RGM \n3- Mostrar todos os alunos \n4- Remover um aluno \n5- Sair do sistema");
-		@SuppressWarnings("resource")
-		Scanner entrada = new Scanner(System.in);
-		int n = entrada.nextInt();
+				"Grupo: Antônio Albuquerque de Oliveira Neto; Júlio César Toscano Ximenes Júnior; Marcelo Camilo Gomes; Marco Túlio Souto Maior Duarte.\n");
 
-		switch (n) {
-		case 1:
+		int n = 0;
+		do {
+			System.out.println(
+					"\n1- Cadastrar aluno \n2- Buscar aluno via RGM \n3- Mostrar todos os alunos \n4- Remover um aluno \n5- Sair do sistema");
+			@SuppressWarnings("resource")
+			Scanner entrada = new Scanner(System.in);
+			n = entrada.nextInt();
+			entrada.nextLine();
 
-		case 2:
-			System.out.println("Informe o RGM para busca: ");
-			int i = entrada.nextInt();
-			listasequencial.buscar(i);
-		case 3:
-			listasequencial.exibirLista();
-		case 4:
-			System.out.println("Informe o RGM do aluno a ser removido: ");
-			int j = entrada.nextInt();
-			listasequencial.removerAluno(j);
-		case 5:
-		default:
-		}
+			String saida = "1";
 
-		Aluno a1 = new Aluno();
-		a1.nome = "Joao";
-		a1.rgm = "01";
+			switch (n) {
+			case 1:
+				System.out.println("Cadastrando aluno.");
+				Aluno alunoTemp = new Aluno();
+				System.out.println("Informe o RGM: ");
+				alunoTemp.rgm = entrada.nextLine();
 
-		Aluno a2 = new Aluno();
-		a2.nome = "Maria";
-		a2.rgm = "02";
+				ListaEncadeada listaDisciplinasTemp = new ListaEncadeada();
 
-		Aluno a3 = new Aluno();
-		a3.nome = "Jose";
-		a3.rgm = "03";
+				do {
 
-		listasequencial.inserirAluno(0, a1);
-		listasequencial.inserirAluno(1, a2);
-		listasequencial.inserirAluno(2, a3);
+					Disciplina disciplinaTemp = new Disciplina();
+					System.out.println("Cadastre uma disciplina: \n0- para sair");
 
-		listasequencial.exibirLista();
-		System.out.println("A lista tem " + listasequencial.tamanhoLista() + " elementos");
+					saida = entrada.next();
+					if (saida.equals("0")) {
+						break;
+					}
+					disciplinaTemp.nome = saida;
+
+					System.out.println("Nota da disciplina: ");
+					disciplinaTemp.nota = entrada.next();
+
+					listaDisciplinasTemp.inserirNoInicio(disciplinaTemp);
+				} while (saida != "0");
+
+				alunoTemp.disciplinas = listaDisciplinasTemp;
+
+				listasequencial.inserirAlunoOrdenado(alunoTemp);
+
+				break;
+			case 2:
+				System.out.println("Informe o RGM para busca: ");
+				String i = entrada.nextLine();
+
+				alunoTemp = listasequencial.buscarPorRgm(i);
+
+				if (alunoTemp != null) {
+					System.out.println("As disciplinas do aluno são: \n");
+					alunoTemp.disciplinas.exibirLista();
+				} else {
+					System.out.println("Aluno não está na lista");
+				}
+				break;
+			case 3:
+
+				for (int y = 0; y < listasequencial.tamanho; y++) {
+					System.out.println("\nRGM: " + listasequencial.alunos[y].getRgm() + "\nDisciplinas: \n ");
+					listasequencial.alunos[y].disciplinas.exibirLista();
+				}
+
+				break;
+			case 4:
+				System.out.println("Informe o RGM do aluno a ser removido: ");
+				String j = entrada.nextLine();
+				listasequencial.removerAluno(j);
+				break;
+			case 5:
+				System.out.println("Saindo do sistema");
+				break;
+			default:
+				System.out.println("Pare de tentar quebrar meu código");
+			}
+		} while (n != 5);
 
 	}
 
